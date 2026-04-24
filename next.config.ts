5491d0a72cbe80fe7b43d665d6adda4105d3be64
Content Rewards AI <support@contentrewardsai.com>
2026-04-24 05:02:31 -0400

    Update URLs

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,23 @@ import { withWhopAppConfig } from "@whop/react/next.config";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	images: {
 		remotePatterns: [{ hostname: "**" }],
 	},
+	rewrites: async () => [
+		{
+			source: "/api/ext-auth/:path*",
+			destination: "/api/ghl/external-auth/:path*",
+		},
+		{
+			source: "/api/ext-sso",
+			destination: "/api/ghl/sso",
+		},
+		{
+			source: "/api/ext-hooks",
+			destination: "/api/ghl/webhooks",
+		},
+	],
 };
 
 export default withWhopAppConfig(nextConfig);
